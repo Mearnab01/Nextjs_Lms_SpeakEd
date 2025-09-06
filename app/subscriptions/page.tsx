fd86ccecd8974459d6fd7250f8d27a629ed46c38
Arnab <Narnabnath919@gmail.com>
2025-09-06 13:55:31 +0530

    Initial Commit

---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,9 @@
+import { PricingTable } from "@clerk/nextjs";
+
+export default function SubscriptinPage() {
+  return (
+    <main>
+      <PricingTable />
+    </main>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,73 @@
+export const subjects = [
+  "maths",
+  "language",
+  "science",
+  "history",
+  "coding",
+  "economics",
+];
+
+export const subjectsColors = {
+  science: "#A78BFA", // Soft violet
+  maths: "#FACC15", // Bright amber
+  language: "#38BDF8", // Sky blue
+  coding: "#F472B6", // Pink
+  history: "#FBBF24", // Warm gold
+  economics: "#34D399", // Emerald green
+};
+
+export const voices = {
+  male: { casual: "2BJW5coyhAzSr8STdHbE", formal: "c6SfcYrb2t09NHXiT80T" },
+  female: { casual: "ZIlrSGI4jZqobxRKprJz", formal: "sarah" },
+};
+
+export const recentSessions = [
+  {
+    id: "1",
+    subject: "science",
+    name: "Neura the Brainy Explorer",
+    topic: "The Wonders of Quantum Physics",
+    duration: 45,
+    color: "#A78BFA", // Soft violet
+  },
+  {
+    id: "2",
+    subject: "maths",
+    name: "Countsy the Number Wizard",
+    topic: "Cracking Calculus: Limits & Derivatives",
+    duration: 30,
+    color: "#FACC15", // Bright amber
+  },
+  {
+    id: "3",
+    subject: "language",
+    name: "Verba the Wordsmith",
+    topic: "Mastering English Idioms & Expressions",
+    duration: 30,
+    color: "#38BDF8", // Sky blue
+  },
+  {
+    id: "4",
+    subject: "coding",
+    name: "Codey the Logic Hacker",
+    topic: "Building Your First JavaScript Game",
+    duration: 45,
+    color: "#F472B6", // Pink
+  },
+  {
+    id: "5",
+    subject: "history",
+    name: "Memo the Memory Keeper",
+    topic: "The Rise and Fall of Ancient Empires",
+    duration: 15,
+    color: "#FBBF24", // Warm gold
+  },
+  {
+    id: "6",
+    subject: "economics",
+    name: "The Market Maestro",
+    topic: "Global Trade and Its Impact on Society",
+    duration: 10,
+    color: "#34D399", // Emerald green
+  },
+];
